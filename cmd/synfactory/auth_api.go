da9d7140734ae248f838f7bc552bee9fcea4ca51
Hoang Hong Huy <111640702+hoanghonghuy@users.noreply.github.com>
2026-09-05 19:09:35 +0700

    feat: wire production GitHub OAuth login

--- cmd/synfactory/auth_api.go
+++ cmd/synfactory/auth_api.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/hoanghonghuy/synfactory/internal/authapi"
 	"github.com/hoanghonghuy/synfactory/internal/authz"
+	"github.com/hoanghonghuy/synfactory/internal/config"
 	"github.com/hoanghonghuy/synfactory/internal/postgres"
 )
 
-func registerAuthAPI(mux *http.ServeMux, store *postgres.Store, authorizer authz.RequestAuthorizer) {
+func registerAuthAPI(mux *http.ServeMux, store *postgres.Store, authorizer authz.RequestAuthorizer, cfg config.Config) {
+	issuer := authz.SessionIssuer{Store: store}
 	handler := authapi.Handler{
 		Store:      store,
 		Authorizer: authorizer,
-		Issuer:     authz.SessionIssuer{Store: store},
+		Issuer:     issuer,
 	}
 	handler.Register(mux)
+
+	if cfg.GitHubOAuthClientID == "" {
+		return
+	}
+	oauth := authapi.OAuthHandler{
+		Store: store,
+		Provider: authz.GitHubOAuthProvider{
+			ClientID:     cfg.GitHubOAuthClientID,
+			ClientSecret: cfg.GitHubOAuthClientSecret,
+			Client:       &http.Client{Timeout: 15 * time.Second},
+		},
+		Issuer:       issuer,
+		ClientID:     cfg.GitHubOAuthClientID,
+		AuthorizeURL: "https://github.com/login/oauth/authorize",
+		RedirectURI:  cfg.GitHubOAuthRedirectURI,
+		ReturnPath:   "/",
+	}
+	oauth.Register(mux)
 }
